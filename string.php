8fab0ad08926836f62c8728d2904179dc44dd068
Julius <Julius>
2020-06-09 12:47:38 +0300

    String Task 1.

---
+++ string.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String</title>
+</head>
+
+<body>
+    <?php
+    # 1. Task ##############
+
+    $vardas = 'Julius';
+    $pavarde = 'Pavardenis';
+    echo '<b>1. </b>';
+    echo "Veikėjas: $vardas $pavarde | ";
+
+    if(strlen($vardas) > strlen($pavarde)) {
+        echo "<b>$vardas</b>";
+    } else {
+        echo "<b>$pavarde</b>";
+    }
+    echo '<br/>';
+    echo "<br/>";
+
+    # 2. Task ##############
+    echo '<b>2. </b>';
+    
+
+    ?>
+
+</body>
+
+</html>
