bcbfac8ca6c1dea82312bbba341b5a0175c2468d
Julius <Julius>
2020-06-10 15:06:46 +0300

    Loop Task 4.

--- loop.php
+++ loop.php
@@ -38,25 +38,48 @@
         }
     } while ($x < $kiek);
 
-        # 2. Task ##############
-        echo '<b>2. </b><br/>';
-        $x = 0;
-        $kiek = 0;
-        do {
-            $rand = rand(0,300);
-            if($rand < 150) {
-                $kiek++;
-            }
-            if($rand >= 275) {
-                echo "<b style='color:red;'>$rand </b>";
-                continue;
-            }
-            echo $rand . "\n";
-            $x++;
-        } while ($x < 300);
-        echo "<br/>";
-        echo "<b>Tarp jų yra: $kiek</b>";
+    echo "<br/>";
+    # 2. Task ##############
+    echo '<b>2. </b><br/>';
+    $x = 0;
+    $kiek = 0;
+    do {
+        $rand = rand(0, 300);
+        if ($rand < 150) {
+            $kiek++;
+        }
+        if ($rand >= 275) {
+            echo "<b style='color:red;'>$rand </b>";
+            continue;
+        }
+        echo $rand . "\n";
+        $x++;
+    } while ($x < 300);
+    echo "<br/>";
+    echo "<b>Tarp jų yra: $kiek</b>";
 
+    echo "<br/>";
+    echo "<br/>";
+    # 3. Task ##############
+    echo '<b>3. </b><br/>';
+
+    $nuo = 0;
+    $iki = 3000;
+    do {
+        $nuo++;
+        if($nuo == 77) {
+            echo $nuo;
+        } else if($nuo % 77 == 0) {
+            echo '<b>,</b> ' . $nuo;
+        }
+
+
+    } while ($nuo < $iki);
+
+    echo "<br/>";
+    echo "<br/>";
+    # 4. Task ##############
+    echo '<b>4. </b><br/>';
 
     ?>
 </body>
