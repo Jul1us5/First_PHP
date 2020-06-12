fa7f86996ec351d63ed6600c8dfe1a4da3c35bea
Julius <Julius>
2020-06-12 23:05:31 +0300

    Array Task 6.

--- array.php
+++ array.php
@@ -13,135 +13,135 @@
 
     $space = "&nbsp;&nbsp;&nbsp;";
 
-        # 1. Task ##############
-        echo '<b>1. </b>';
+    # 1. Task ##############
+    echo '<b>1. </b>';
 
-        $array = array();
-        $size = 30;
-        for ($x = 1; $x <= $size; $x++) {
-            $rand = rand(5, 25);
-            array_push($array, $rand);
+    $array = array();
+    $size = 30;
+    for ($x = 1; $x <= $size; $x++) {
+        $rand = rand(5, 25);
+        array_push($array, $rand);
+    }
+    print("<pre>" . print_r($array, true) . "</pre>");
+
+    echo "<br/>";
+    # 2. Task ##############
+    echo '<b>2.</b>';
+
+    $kiek = 0;
+    $suma = 0;
+    $newArray = [];
+    $minus = 0;
+    $poriniai = [];
+    $neporiniai = [];
+
+    foreach ($array as $key => $value) {
+        $suma += $value;
+        if ($value > 10) $kiek++;
+        $minus = $value - $key;
+        array_push($newArray, $minus);
+        if ($key % 2 == 0) {
+            $poriniai[$key] = $value;
+        } else {
+            $neporiniai[$key] = $value;
         }
-        print("<pre>" . print_r($array, true) . "</pre>");
-    
-        echo "<br/>";
-        # 2. Task ##############
-        echo '<b>2.</b>';
-    
-        $kiek = 0;
-        $suma = 0;
-        $newArray = [];
-        $minus = 0;
-        $poriniai = [];
-        $neporiniai = [];
-    
-        foreach ($array as $key => $value) {
-            $suma += $value;
-            if ($value > 10) $kiek++;
-            $minus = $value - $key;
-            array_push($newArray, $minus);
-            if ($key % 2 == 0) {
-                $poriniai[$key] = $value;
-            } else {
-                $neporiniai[$key] = $value;
-            }
+    }
+    echo "<b> a:</b> Yra daugiau <b>| $kiek</b><br/>";
+    $max = max($array);
+    echo "$space<b> b:</b> Didžiausia: <b>| $max</b><br/>";
+    echo "$space<b> c:</b> Suma: <b>| $suma</b><br/>";
+    echo "$space<b> d:</b> Reikšmė minus Indeksas:<br/>";
+    print("<pre>" . print_r($newArray, true) . "</pre>");
+
+    $array = $newArray;
+    $add = 10;
+    for ($x = 1; $x <= $add; $x++) {
+        $rand = rand(5, 25);
+        array_push($array, $rand);
+    }
+    echo "$space<b> e:</b> Plius 10 elementų: <b></b><br/>";
+    print("<pre>" . print_r($array, true) . "</pre>");
+    echo "$space<b> e:</b> Poriniai: <b></b><br/>";
+    print("<pre>" . print_r($poriniai, true) . "</pre>");
+    echo "$space<b> ir </b> Neporiniai: <b></b><br/>";
+    print("<pre>" . print_r($neporiniai, true) . "</pre>");
+
+    $daugiau = 15;
+    $koks = 0;
+    foreach ($array as $key => $value) {
+        if ($value > 15) $array[$key] = '0';
+        else $array[$key] = $value;
+    }
+    echo "$space<b> g:</b><br/>";
+    print("<pre>" . print_r($array, true) . "</pre>");
+
+    foreach ($array as $key => $value) {
+        if ($value > 10) {
+            $koks = $key;
+            break;
         }
-        echo "<b> a:</b> Yra daugiau <b>| $kiek</b><br/>";
-        $max = max($array);
-        echo "$space<b> b:</b> Didžiausia: <b>| $max</b><br/>";
-        echo "$space<b> c:</b> Suma: <b>| $suma</b><br/>";
-        echo "$space<b> d:</b> Reikšmė minus Indeksas:<br/>";
-        print("<pre>" . print_r($newArray, true) . "</pre>");
-    
-        $array = $newArray;
-        $add = 10;
-        for ($x = 1; $x <= $add; $x++) {
-            $rand = rand(5, 25);
-            array_push($array, $rand);
+    }
+    echo "$space<b> h: </b>Mažiausias indeksas, kurio elemento reikšmė didesnė už 10 yra: <b>$koks</b><br/>";
+    echo "<br/>";
+
+    foreach ($array as $key => $value) {
+        if ($key % 2 == 0) {
+            unset($array[$key]);
         }
-        echo "$space<b> e:</b> Plius 10 elementų: <b></b><br/>";
-        print("<pre>" . print_r($array, true) . "</pre>");
-        echo "$space<b> e:</b> Poriniai: <b></b><br/>";
-        print("<pre>" . print_r($poriniai, true) . "</pre>");
-        echo "$space<b> ir </b> Neporiniai: <b></b><br/>";
-        print("<pre>" . print_r($neporiniai, true) . "</pre>");
-    
-        $daugiau = 15;
-        $koks = 0;
-        foreach ($array as $key => $value) {
-            if ($value > 15) $array[$key] = '0';
-            else $array[$key] = $value;
+    }
+    echo "$space<b> i:</b><br/>";
+    print("<pre>" . print_r($array, true) . "</pre>");
+
+    echo "<br/>";
+    # 3. Task ##############
+    echo '<b>3. </b><br/>';
+
+
+    $add = 200;
+    $array = [];
+    for ($x = 1; $x <= $add; $x++) {
+
+        $range = range('A', 'D');
+        $index = array_rand($range);
+        array_push($array, $range[$index]);
+    }
+
+    $a = 0;
+    $b = 0;
+    $c = 0;
+    $d = 0;
+
+
+    foreach ($array as $value) {
+        if ($value == 'A') {
+            $a++;
+        } else if ($value == 'B') {
+            $b++;
+        } else if ($value == 'C') {
+            $c++;
+        } else {
+            $d++;
         }
-        echo "$space<b> g:</b><br/>";
-        print("<pre>" . print_r($array, true) . "</pre>");
-    
-        foreach ($array as $key => $value) {
-            if ($value > 10) {
-                $koks = $key;
-                break;
-            }
-        }
-        echo "$space<b> h: </b>Mažiausias indeksas, kurio elemento reikšmė didesnė už 10 yra: <b>$koks</b><br/>";
-        echo "<br/>";
-    
-        foreach ($array as $key => $value) {
-            if ($key % 2 == 0) {
-                unset($array[$key]);
-            }
-        }
-        echo "$space<b> i:</b><br/>";
-        print("<pre>" . print_r($array, true) . "</pre>");
-    
-        echo "<br/>";
-        # 3. Task ##############
-        echo '<b>3. </b><br/>';
-    
-    
-        $add = 200;
-        $array = [];
-        for ($x = 1; $x <= $add; $x++) {
-    
-            $range = range('A', 'D');
-            $index = array_rand($range);
-            array_push($array, $range[$index]);
-        }
-    
-        $a = 0;
-        $b = 0;
-        $c = 0;
-        $d = 0;
-    
-    
-        foreach ($array as $value) {
-            if ($value == 'A') {
-                $a++;
-            } else if ($value == 'B') {
-                $b++;
-            } else if ($value == 'C') {
-                $c++;
-            } else {
-                $d++;
-            }
-            echo $value . " ";
-        }
-        echo "<br/>";
-        echo "<br/>";
-        echo "<b>A</b> yra: $a<br/>";
-        echo "<b>B</b> yra: $b<br/>";
-        echo "<b>C</b> yra: $c<br/>";
-        echo "<b>D</b> yra: $d<br/>";
-    
-        echo "<br/>";
-        # 4. Task ##############
-        echo '<b>4. </b><br/>';
-    
-        sort($array);
-        foreach ($array as $value) {
-            echo $value . " ";
-        }
-    
-        echo "<br/>";
-        echo "<br/>";
+        echo $value . " ";
+    }
+    echo "<br/>";
+    echo "<br/>";
+    echo "<b>A</b> yra: $a<br/>";
+    echo "<b>B</b> yra: $b<br/>";
+    echo "<b>C</b> yra: $c<br/>";
+    echo "<b>D</b> yra: $d<br/>";
+
+    echo "<br/>";
+    # 4. Task ##############
+    echo '<b>4. </b><br/>';
+
+    sort($array);
+    foreach ($array as $value) {
+        echo $value . " ";
+    }
+
+    echo "<br/>";
+    echo "<br/>";
 
     echo '<b>5. </b>';
 
@@ -177,6 +177,33 @@
     # 6. Task ##############
     echo '<b>6. </b><br/>';
 
+    $array1 = [];
+    $array2 = [];
+    $kiek = 100;
+    $or = 0;
+    $x = 0;
+
+    for ($i = 0; $i < $kiek; $i++) {
+        $random = random_int(100, 999);
+        if (in_array($random, $array1)) $kiek++;
+        else array_push($array1, $random);
+    }
+
+    for ($i = 0; $i < $kiek; $i++) {
+        $random = random_int(100, 999);
+        if (in_array($random, $array2)) $kiek++;
+        else array_push($array2, $random);
+    }
+    echo "<b>Pirmas masyvas: </b>";
+    foreach ($array1 as $value) {
+        echo "$value ";
+    }
+    echo "<br/>";
+    echo "<br/>";
+    echo "<b>Antras masyvas: </b>";
+    foreach ($array2 as $value) {
+        echo "$value ";
+    }
 
     ?>
 
