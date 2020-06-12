88e35faf0ed4c7fc11f52016d474bd0f38cb1378
Julius <Julius>
2020-06-12 13:54:13 +0300

    Array

---
+++ array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Array</title>
+</head>
+
+<body>
+    <?php
+
+    # 1. Task ##############
+    echo '<b>1. </b><br/>';
+
+
+    ?>
+
+</body>
+
+</html>
