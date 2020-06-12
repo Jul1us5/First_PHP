cc40bc528138dfa45c90d9e6696ceb3493d5b6c0
Julius <Julius>
2020-06-12 14:16:50 +0300

    Array Task 1.

--- array.php
+++ array.php
@@ -10,7 +10,19 @@
     <?php
 
     # 1. Task ##############
-    echo '<b>1. </b><br/>';
+    echo '<b>1. </b>';
+
+    $array = array();
+    $size = 30;
+    for ($x = 1; $x <= $size; $x++) {
+        $rand = rand(5, 25);
+        array_push($array, $rand);
+    }
+    print("<pre>".print_r($array,true)."</pre>");
+
+    echo "<br/>";
+    # 2. Task ##############
+    echo '<b>2. </b><br/>';
 
 
     ?>
