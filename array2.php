2cd78c0a76460fe6b9cc74390c59132222f9f8ba
Julius <Julius>
2020-06-15 02:00:24 +0300

    Array 2 Task 11...

--- array2.php
+++ array2.php
@@ -288,9 +288,27 @@
         }
     }
     echo "</div>";
-
-
     print("<pre>" . print_r($multiArray, true) . "</pre>");
+
+    echo "<br/>";
+    # 11. Task ##############
+    echo '<b>11.</b>';
+
+    do {
+        $a = rand(0, 1000);
+        $b = rand(0, 1000);
+    } while ($a == $b);
+    $long = rand(10,30);
+    $sk1 = $sk2 = 0;
+    echo '<h3>Skaičiai '.$a.' ir '.$b.'</h3>';
+    $c = [];
+    for ($i=0; $i<$long; $i++) {
+        $c[] = array_rand(array_flip([$a, $b]));
+    }
+    echo '<h4>Masyvas:</h4>';
+    echo '<pre>';
+    print_r($c);
+    echo '</pre>';
 
     ?>
 </body>
