228b8cb8d1d99f8083a36c6d88ac927baea1a508
Julius <Julius>
2020-06-15 23:04:58 +0300

    Function Task 1 and 2

--- function.php
+++ function.php
@@ -7,12 +7,39 @@
     <title>Function</title>
 </head>
 
+<style>
+    body * {
+        box-sizing: border-box;
+        display: inline-block;
+        padding: 0;
+        margin: 0;
+    }
+</style>
+
+
 <body>
     <?php
 
+    // Setting
 
+    $space = "&nbsp;&nbsp;&nbsp;";
 
+    # 1. Task ##############
+    echo '<b>1. ir 2. </b><br/>';
+    echo "<br/>";
 
+    function tags($text, $h)
+    {
+        return "<h$h>$text</h$h>";
+    }
+    echo tags('Labas Pasauli!', '1');
+
+    echo "<br/>";
+    echo "<br/>";
+    # 3. Task ##############
+    echo '<b>3.</b>';
+
+    
     ?>
 </body>
 
